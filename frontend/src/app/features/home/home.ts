e00cfcb7c8c23a2a41f960e54b53c46c1ad2b387
Bennet Steem <bennet.steem@gmail.com>
2026-03-06 11:52:00 -0700

    add projects and contact features to frontend

--- frontend/src/app/features/home/home.ts
+++ frontend/src/app/features/home/home.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../shared/components/navbar/navbar';
 import { Footer } from '../../shared/components/footer/footer';
-
+import { Projects } from "../projects/projects";
+import { Contact } from "../contact/contact";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer, Projects, Contact],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
