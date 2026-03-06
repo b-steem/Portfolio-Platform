868b7b7f361c54efc1b304fc9354a67dc74480f5
Bennet Steem <bennet.steem@gmail.com>
2026-03-06 11:32:08 -0700

    Add home hero section and navbar to frontend

---
+++ frontend/src/app/shared/components/navbar/navbar.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+}
