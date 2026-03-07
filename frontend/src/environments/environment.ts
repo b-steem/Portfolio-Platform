cdd24e2efc9335c61e90be3c5b1393ceba1269d7
Bennet Steem <bennet.steem@gmail.com>
2026-03-07 14:50:31 -0700

    added frontend URL to backend settings

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,9 @@
+// export const environment = {
+//   production: false,
+//   apiBaseUrl: 'http://localhost:5250'
+// };
+
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:5250'
+  production: true,
+  apiBaseUrl: 'portfolio-platform-api-bsteem-apcjandkbxbaabe3.canadacentral-01.azurewebsites.net'
 };
